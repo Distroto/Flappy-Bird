80ff9308673cfa52caa30b759f0248fda0a94924
Abhishek Sokhal <abhisheksokhal2035@gmail.com>
2022-08-17 21:51:05 +0530

    initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float spawnRate = 1f;
+
+    public float minheight = -1f;
+
+    public float maxheight = 1f;
+
+    private void OnEnable() {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+
+    private void OnDisable() {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn() {
+        GameObject Pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+        Pipes.transform.position += Vector3.up * Random.Range(minheight, maxheight); 
+    }
+}
